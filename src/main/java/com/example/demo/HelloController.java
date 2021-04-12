2fd216649f79a18ce3ca3649800af2af52a4cab9
ShwetaMore <moreshweta40@gmail.com>
2021-04-13 00:57:00 +0530

    Method name change

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	@RequestMapping("/greetings")
-	public String greetings(){
+	public String hello(){
 		return "My First Spring Boot Application!!";
 	}
 }
